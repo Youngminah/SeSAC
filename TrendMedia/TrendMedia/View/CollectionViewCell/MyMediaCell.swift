84815f913879e2a662a06df8557d19d215a58ba7
Youngminah <bluebelsmudi@gmail.com>
2021-10-19 22:42:29 +0900

    [TrendMedia] Feat: mymedia scene

--- TrendMedia/TrendMedia/View/CollectionViewCell/MyMediaCell.swift
+++ TrendMedia/TrendMedia/View/CollectionViewCell/MyMediaCell.swift
@@ -9,13 +9,68 @@ import UIKit
 
 class MyMediaCell: UICollectionViewCell {
     
+    static let identifier = "MyMediaCell"
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test Title"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "squid_game")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let rateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0.0"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 13, weight: .medium)
+        label.textAlignment = .center
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .clear
-        //setConstraint()
+        self.contentView.backgroundColor = .systemTeal
+        self.contentView.layer.cornerRadius = 10
+        self.contentView.layer.masksToBounds = true
+        setConstraint()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraint() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(posterImageView)
+        contentView.addSubview(rateLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(10)
+            make.left.equalTo(contentView.safeAreaInsets.left).offset(16)
+            make.right.equalTo(contentView.safeAreaInsets.right).offset(-16)
+            make.height.equalTo(25)
+        }
+        
+        posterImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.right.equalTo(contentView.safeAreaLayoutGuide).offset(-16)
+            make.bottom.equalTo(contentView.safeAreaLayoutGuide)
+            make.width.equalTo(100)
+        }
+        
+        rateLabel.snp.makeConstraints { make in
+            make.left.equalTo(contentView.safeAreaInsets.left).offset(16)
+            make.bottom.equalTo(contentView.safeAreaInsets.right).offset(-16)
+            make.right.equalTo(posterImageView.snp.left).offset(-16)
+            make.height.equalTo(40)
+        }
     }
 }
--- TrendMedia/TrendMedia/ViewController/MyMediaViewController.swift
+++ TrendMedia/TrendMedia/ViewController/MyMediaViewController.swift
@@ -8,11 +8,80 @@
 import UIKit
 
 class MyMediaViewController: UIViewController {
+    
+    
+    private let collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addView()
+        setConstraints()
+        setConfiguration()
+    }
+    
+    private func setConfiguration() {
         view.backgroundColor = .white
         title = "MY MEDIA"
         self.navigationController?.navigationBar.tintColor = .label
+        setCollectionView()
+    }
+    
+    private func setCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MyMediaCell.self, forCellWithReuseIdentifier: MyMediaCell.identifier)
+    }
+    
+    private func addView() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.equalTo(view.safeAreaInsets.left)
+            make.right.equalTo(view.safeAreaInsets.right)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension MyMediaViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyMediaCell.identifier, for: indexPath) as? MyMediaCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+}
+
+extension MyMediaViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (UIScreen.main.bounds.width - ( 16 * 2 ))/2
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
     }
 }
