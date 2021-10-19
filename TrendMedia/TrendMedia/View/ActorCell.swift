27c1910faf5608d801b214aa02bcd0569d942869
Youngminah <bluebelsmudi@gmail.com>
2021-10-19 18:10:21 +0900

    [TrendMedia] Feat: actor scene

--- TrendMedia/TrendMedia/View/ActorCell.swift
+++ TrendMedia/TrendMedia/View/ActorCell.swift
@@ -10,15 +10,67 @@ import UIKit
 class ActorCell: UITableViewCell {
     
     static let identifier = "ActorCell"
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "스네이프")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "앨럭 릭먼"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    private let roleNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "스네이프 교수"
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        return label
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [nameLabel, roleNameLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         selectionStyle = .none
-        //setConstraint()
+        setConstraint()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    
+    private func setConstraint() {
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(stackView)
+        
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(10)
+            make.left.equalTo(contentView.safeAreaInsets.left).offset(16)
+            make.bottom.equalTo(contentView.safeAreaInsets.bottom).offset(-10)
+            make.width.equalTo(profileImageView.snp.height)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.left.equalTo(profileImageView.snp.right).offset(16)
+            make.right.equalTo(contentView.safeAreaInsets.right).offset(-16)
+            make.centerY.equalTo(contentView.snp.centerY)
+        }
+        
+    }
 }
--- TrendMedia/TrendMedia/ViewController/ActorViewController.swift
+++ TrendMedia/TrendMedia/ViewController/ActorViewController.swift
@@ -8,11 +8,63 @@
 import UIKit
 
 class ActorViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.showsVerticalScrollIndicator = false
+        tableView.backgroundColor = .clear
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 90, bottom: 0, right: 16)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addView()
+        setConstraints()
+        setConfiguration()
+    }
+    
+    private func setConfiguration() {
         self.view.backgroundColor = .white
         self.title = "출연/제작"
         self.navigationController?.navigationBar.tintColor = .label
+        setTableView()
+    }
+    
+    private func addView() {
+        view.addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.equalTo(view.safeAreaInsets.left)
+            make.right.equalTo(view.safeAreaInsets.right)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ActorCell.self, forCellReuseIdentifier: ActorCell.identifier)
+    }
+}
+
+extension ActorViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ActorCell.identifier, for: indexPath) as? ActorCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
