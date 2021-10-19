be44d42af5c73787b699d785ad81aed1ea8eeb6a
Youngminah <bluebelsmudi@gmail.com>
2021-10-19 14:29:07 +0900

    [TrendMedia] Add: vc

---
+++ TrendMedia/TrendMedia/ViewController/ActorViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ActorViewController.swift
+//  TrendMedia
+//
+//  Created by meng on 2021/10/19.
+//
+
+import UIKit
+
+class ActorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ TrendMedia/TrendMedia/ViewController/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  TrendMedia
+//
+//  Created by meng on 2021/10/19.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
