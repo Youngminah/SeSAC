1378eb97f1db5f6abe592c87c5d02cdd8102b003
Youngminah <bluebelsmudi@gmail.com>
2021-10-19 19:16:00 +0900

    [TrendMedia] Add: model

---
+++ TrendMedia/TrendMedia/ViewModel/MediaViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MediaViewModel.swift
+//  TrendMedia
+//
+//  Created by meng on 2021/10/19.
+//
+
+import Foundation
+
+final class MediaViewModel {
+    let tvShowList: [TvShow] = DataManager.tvShow
+}
