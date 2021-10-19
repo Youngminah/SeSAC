e040b3638cec12a290de1f7eec22cde21cd66178
Youngminah <bluebelsmudi@gmail.com>
2021-10-19 20:45:29 +0900

    [TrendMedia] Add: mymedia cell

---
+++ TrendMedia/TrendMedia/View/CollectionViewCell/MyMediaCell.swift
@@ -0,0 +1,21 @@
+//
+//  MyMediaCell.swift
+//  TrendMedia
+//
+//  Created by meng on 2021/10/19.
+//
+
+import UIKit
+
+class MyMediaCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .clear
+        //setConstraint()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
