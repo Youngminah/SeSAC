a320658bef7bb553c311715394c1bf9cf540bec9
Youngminah <bluebelsmudi@gmail.com>
2021-10-19 12:31:08 +0900

    [TrendMedia] Initial setting

---
+++ TrendMedia/TrendMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendMedia
+//
+//  Created by meng on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
